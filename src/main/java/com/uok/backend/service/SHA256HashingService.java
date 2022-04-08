e81c6864e70a81db217fa181ed0e9ac6aba6715e
Krishan Shamod <krishanshamod@gmail.com>
2022-04-08 12:26:18 +0530

    Added Hashing service

---
+++ src/main/java/com/uok/backend/service/SHA256HashingService.java
@@ -0,0 +1,29 @@
+package com.uok.backend.service;
+
+import org.springframework.stereotype.Component;
+
+import java.security.NoSuchAlgorithmException;
+
+@Component
+public class SHA256HashingService implements HashingService {
+    @Override
+    public String hash(String input) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("SHA-256");
+            byte[] messageDigest = md.digest(input.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : messageDigest) {
+                hexString.append(String.format("%02X", 0xFF & b));
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    @Override
+    public boolean verify(String input, String hash) {
+        return hash.equals(hash(input));
+    }
+}

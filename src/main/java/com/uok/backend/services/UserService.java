364017e4d25064cd94f71d23c08b1ecf39aca453
Krishan Shamod <krishanshamod@gmail.com>
2022-04-08 20:21:58 +0530

    Added token genertion functionality to user service

--- src/main/java/com/uok/backend/services/UserService.java
+++ src/main/java/com/uok/backend/services/UserService.java
@@ -5,5 +5,5 @@ import com.uok.backend.domains.UserSignInDetails;
 
 public interface UserService {
     public boolean signUp(User user);
-    public boolean signIn(UserSignInDetails userSignInDetails);
+    public String signIn(UserSignInDetails userSignInDetails);
 }

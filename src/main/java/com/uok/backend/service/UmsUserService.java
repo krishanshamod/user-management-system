5750cb0b958965dda463ef9193c86fa2cf18816b
Krishan Shamod <krishanshamod@gmail.com>
2022-04-08 12:28:33 +0530

    Added password hashing and user adding functionalities to user service

--- src/main/java/com/uok/backend/service/UmsUserService.java
+++ src/main/java/com/uok/backend/service/UmsUserService.java
@@ -6,19 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserServiceImpl implements UserService {
+public class UmsUserService implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private HashingService hashingService;
 
 
     @Override
     public boolean signup(User user) {
         String email = user.getEmail();
+        String password = user.getPassword();
 
+        // hash the password and set it to the user
+        String hashedPassword = hashingService.hash(password);
+        user.setPassword(hashedPassword);
+
+        // check if the user already exists or not and add user to the database
         if (userRepository.findByEmail(email) == null) {
-            //userRepository.save(user);
-            System.out.println("User registered");
+            userRepository.save(user);
             return true;
         } else {
             return false;
--- src/main/java/com/uok/backend/service/UserService.java
+++ src/main/java/com/uok/backend/service/UserService.java
@@ -3,7 +3,5 @@ package com.uok.backend.service;
 import com.uok.backend.domain.User;
 
 public interface UserService {
-
     public boolean signup(User user);
-
 }

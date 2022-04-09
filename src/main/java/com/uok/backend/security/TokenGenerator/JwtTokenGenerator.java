d44a16c1a8f9b1c7cf8b847d4a4465b4b9cf1aa5
Krishan Shamod <krishanshamod@gmail.com>
2022-04-09 23:46:09 +0530

    Changed the JWT secret location

--- src/main/java/com/uok/backend/security/TokenGenerator/JwtTokenGenerator.java
+++ src/main/java/com/uok/backend/security/TokenGenerator/JwtTokenGenerator.java
@@ -2,14 +2,12 @@ package com.uok.backend.security.TokenGenerator;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenGenerator implements TokenGenerator {
 
-    @Value("${jwt.secret}")
-    private String secret;
+    private String secret = "OurLittleSecretIsHere";
 
     @Override
     public String generate(String email, String firstName, String lastName, String role) {

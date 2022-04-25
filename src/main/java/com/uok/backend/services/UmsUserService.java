3adff10ad80a20af71aae21d312cbdcca8f5a960
Krishan Shamod <krishanshamod@gmail.com>
2022-04-25 21:41:03 +0530

    Refactored signIn method code

--- src/main/java/com/uok/backend/services/UmsUserService.java
+++ src/main/java/com/uok/backend/services/UmsUserService.java
@@ -9,6 +9,7 @@ import com.uok.backend.exceptions.FailedRegistrationException;
 import com.uok.backend.repositories.UserRepository;
 import com.uok.backend.security.PasswordGenerator.HashedPasswordGenerator;
 import com.uok.backend.security.TokenGenerator.TokenGenerator;
+import com.uok.backend.utils.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -24,17 +25,28 @@ public class UmsUserService implements UserService {
 
     private TokenGenerator tokenGenerator;
 
+    private Logger logger;
+
     @Autowired
-    public UmsUserService(UserRepository userRepository, HashedPasswordGenerator hashedPasswordGenerator
-            , TokenGenerator tokenGenerator) {
+    public UmsUserService(
+            UserRepository userRepository,
+            HashedPasswordGenerator hashedPasswordGenerator,
+            TokenGenerator tokenGenerator,
+            Logger logger) {
+
         this.userRepository = userRepository;
         this.hashedPasswordGenerator = hashedPasswordGenerator;
         this.tokenGenerator = tokenGenerator;
+        this.logger = logger;
     }
 
 
     @Override
     public ResponseEntity signUp(User user) {
+
+        // log api call
+        logger.logApiCall("signUp");
+
         String email = user.getEmail();
         String password = user.getPassword();
 
@@ -57,16 +69,23 @@ public class UmsUserService implements UserService {
             // add user to the database
             userRepository.save(user);
 
+            // log success message
+            logger.logSuccess("signUp");
+
             return ResponseEntity.ok(null);
 
         } catch (FailedRegistrationException | DataMissingException e) {
-            System.out.println(e.getMessage());
+            logger.logException(e.getMessage());
             return ResponseEntity.status(HttpServletResponse.SC_BAD_REQUEST).body(null);
         }
     }
 
     @Override
     public ResponseEntity signIn(SignInRequest signInRequest) {
+
+        // log api call
+        logger.logApiCall("signIn");
+
         String email = signInRequest.getEmail();
         String password = signInRequest.getPassword();
 
@@ -77,38 +96,38 @@ public class UmsUserService implements UserService {
             }
 
             // check if the user exists or not
-            if (userRepository.findByEmail(email) != null) {
-
-                // get hashed password from the database
-                String hashedPassword = userRepository.findByEmail(email).getPassword();
-
-                // check if the password matches
-                boolean isPasswordCorrect = hashedPasswordGenerator.verify(password, hashedPassword);
-
-                if (isPasswordCorrect) {
-
-                    // generate token
-                    String token = tokenGenerator.generate(
-                            userRepository.findByEmail(email).getEmail(),
-                            userRepository.findByEmail(email).getFirstName(),
-                            userRepository.findByEmail(email).getLastName(),
-                            userRepository.findByEmail(email).getRole()
-                    );
-
-                    return ResponseEntity.ok(new SignInResponse(token));
-
-                } else {
-                    throw new FailedLoginException("Invalid password");
-                }
-            } else {
+            if (userRepository.findByEmail(email) == null) {
                 throw new FailedLoginException("User does not exist");
             }
 
+            // get hashed password from the database
+            String hashedPassword = userRepository.findByEmail(email).getPassword();
+
+            // check if the password matches
+            boolean isPasswordCorrect = hashedPasswordGenerator.verify(password, hashedPassword);
+
+            if (!isPasswordCorrect) {
+                throw new FailedLoginException("Invalid password");
+            }
+
+            // generate token
+            String token = tokenGenerator.generate(
+                    userRepository.findByEmail(email).getEmail(),
+                    userRepository.findByEmail(email).getFirstName(),
+                    userRepository.findByEmail(email).getLastName(),
+                    userRepository.findByEmail(email).getRole()
+            );
+
+            // log success message
+            logger.logSuccess("signIn");
+
+            return ResponseEntity.ok(new SignInResponse(token));
+
         } catch (FailedLoginException e) {
-            System.out.println(e.getMessage());
+            logger.logException(e.getMessage());
             return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(null);
         } catch (DataMissingException e) {
-            System.out.println(e.getMessage());
+            logger.logException(e.getMessage());
             return ResponseEntity.status(HttpServletResponse.SC_BAD_REQUEST).body(null);
         }
     }

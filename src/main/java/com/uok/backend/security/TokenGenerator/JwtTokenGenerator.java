78a5417bd98e9c4cfc7c2442365f159ab83286ea
Krishan Shamod <krishanshamod@gmail.com>
2022-04-26 17:42:32 +0530

    Moved the token secret from code to env variable

--- src/main/java/com/uok/backend/security/TokenGenerator/JwtTokenGenerator.java
+++ src/main/java/com/uok/backend/security/TokenGenerator/JwtTokenGenerator.java
@@ -2,25 +2,32 @@ package com.uok.backend.security.TokenGenerator;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenGenerator implements TokenGenerator {
 
-    private String secret = "OurLittleSecretIsHere";
+    private Environment env;
+    private String secret;
+
+    @Autowired
+    public JwtTokenGenerator(Environment env) {
+        this.env = env;
+        this.secret = env.getProperty("token.secret");
+    }
 
     @Override
     public String generate(String email, String firstName, String lastName, String role) {
 
-        // Generate token
-        String token = Jwts.builder()
+        // Generate and return token
+        return Jwts.builder()
                 .claim("email", email)
                 .claim("firstName", firstName)
                 .claim("lastName", lastName)
                 .claim("role", role)
                 .signWith(SignatureAlgorithm.HS512, secret )
                 .compact();
-
-        return token;
     }
 }

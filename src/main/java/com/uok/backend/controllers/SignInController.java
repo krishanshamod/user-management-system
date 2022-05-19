db2e889f63c02ac7f424b0ff3047163b902c59ae
Krishan Shamod <krishanshamod@gmail.com>
2022-05-19 05:32:40 +0530

    Removed unnecessary imports

--- src/main/java/com/uok/backend/controllers/SignInController.java
+++ src/main/java/com/uok/backend/controllers/SignInController.java
@@ -1,15 +1,12 @@
 package com.uok.backend.controllers;
 
 import com.uok.backend.domains.SignInRequest;
-import com.uok.backend.domains.SignInResponse;
 import com.uok.backend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class SignInController {
